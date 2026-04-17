f45e2f41de832b200a5517ec155debbac9efa96a
racenkai123 <racenkai@hawaii.edu>
2026-04-17 04:46:43 -1000

    finished

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 import './styles.sass';
 import './example_typescript';
+
+
+const app = document.createElement("div");
+app.innerHTML = "<h1>Hello world!! Webpack is working</h1>";
+
+document.body.appendChild(app);
